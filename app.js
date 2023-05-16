4e6428e3c19d8927d4c3063664292269c69d3b9b
shivarajkumar <shivaraj1654@gmail.com>
2023-05-16 12:30:15 +0530

    error page

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminrouter);
+app.use('/admin',adminrouter);
 app.use(shopRoute);
+
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req,res,next) => {
-    res.send('<form action="/product" method="POST"><label>title: </label><input type="text" name="title"><label>size: </label><input type="number" name="size"><button type="submit">Add Product</button></form>')
+    res.send('<form action="/admin/add-product" method="POST"><label>title: </label><input type="text" name="title"><label>size: </label><input type="number" name="size"><button type="submit">Add Product</button></form>')
     console.log('in the last middleware');
 });
 
-router.post('/product', (req,res,next) => {
+router.post('/add-product', (req,res,next) => {
     console.log(req.body) ;
     res.redirect('/');
 });
