ec08c4aab30f0c214a35e3d063cc8f0501b9d00f
shivarajkumar <shivaraj1654@gmail.com>
2023-05-18 15:54:06 +0530

    book a call

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,12 +8,13 @@ const adminrouter = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin',adminrouter);
 app.use(shopRoute);
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
-})
+    res.status(404).sendFile(path.join(__dirname, './', 'views', 'error-page.html'));
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
-
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/add-product', (req,res,next) => {
-    res.send('<form action="/admin/add-product" method="POST"><label>title: </label><input type="text" name="title"><label>size: </label><input type="number" name="size"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
     console.log('in the last middleware');
 });
 
@@ -11,5 +12,18 @@ router.post('/add-product', (req,res,next) => {
     console.log(req.body) ;
     res.redirect('/');
 });
+
+router.get('/success', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'successful.html'));
+  });
+  
+  router.get('/bookcall', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'bookcall.html'));
+  });
+  
+  router.post('/bookcall', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('successfull');
+  });
 
 module.exports = router;
